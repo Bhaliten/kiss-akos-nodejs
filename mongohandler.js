dadb22453eceeb5ec30e63f8cfdef421e5c48439
simon <bhaliten@gmail.com>
2022-06-22 20:46:21 +0200

    wait

--- mongohandler.js
+++ mongohandler.js
@@ -1,24 +1,44 @@
 const MongoClient = require('mongodb').MongoClient;
+
+//Ez a localis mongodb elérés, ez akkor fog működni ha te magad futtatod az adatbázist is
 // const url = "mongodb://localhost:27017/";
+
+//ez egy általam létrehozott adatbázis, hogy ne kelljen localba futtatnom :D
+//szóval ha van localon adatbázisod, akkor tedd vissza fentebbi localost, de használhatod ezt is
 const url = "mongodb+srv://Simon:asd@cluster0.tgzs7xi.mongodb.net/?retryWrites=true&w=majority";
 const dbName = 'mydb'
 
-const dropCollection = function (collectionName, callback) {
+
+//Ezzel a függvémmyel törölhetsz egy collection-t
+//Az 1. paramétere a collection, a második egy meghívható függvény, ami akkor lesz meghívva, ha sikeresen töröltük a collectiont
+const dropOurCollection = function (collectionName, callback) {
+
+    //csatlakozunk a MongoClient segítségével a fentebb megadott url-re, azaz az adatbázisra
     MongoClient.connect(url, function (err, mongoClient) {
+
+        //Ha nem sikerült a csatlakozás, akkor eldobjuk a hibát, és nem fut tovább a kód (kiírja a hibát a consolera)
         if (err) throw err;
 
+        //Az adott adatbázis objektumát elkérjük a .db függvény segítségével
         const database = mongoClient.db(dbName);
+
+        //az adatbázis kliensével kitörlöm a collectiont
         database.dropCollection(collectionName)
+
+            //ha sikerült a törlés, akkor mindig a then() rész fog lefutni
             .then(result => {
                 //ahol látsz ilyen callback függvény hívást, ott a másik fájlba hívunk vissza
                 callback();
                 console.log("Collection deleted!");
             })
+            //ha problémába ütközünk törlés közben, akkor logolunk
             .catch(err => {
-                console.log("Collection is still exists!");
+                console.log("Collection not deleted!");
                 throw err;
 
             })
+
+            //A finally rész minden esetben le fog futni, ahol általában a connectiont zárjuk le
             .finally(() => {
                 mongoClient.close();
             });
@@ -26,18 +46,26 @@ const dropCollection = function (collectionName, callback) {
 }
 
 
+//Ezzel a függvénnyel ellenörizhetjük azt, hogy az adott collection létezik e
+//1. paraméter a keresendő collection, a 2. az a függvény ami meg lesz hívva, ha létezik
+// a 3. paraméter pedig szintén egy függvény, amit akkor fogunk hívni, ha nem lézetik az adott collection
 const collectionExists = function (collectionName, exists, notExistsCallback) {
     MongoClient.connect(url, function (err, mongoClient) {
         if (err) throw err;
 
         const database = mongoClient.db(dbName);
+
+        //a listCollections vissza adná a collectionöket, de mi ezt filtereljük name alapján
         database.listCollections({name: collectionName})
             .next(function (err, result) {
                 mongoClient.close();
+
+                //ha a result nem null, akkor bemegy, és meghívja a 2. paraméterként átadott függvémyt
                 if (result) {
                     //ahol látsz ilyen callback függvény hívást, ott a másik fájlba hívunk vissza
                     exists();
                 } else {
+                    //egyébként meg a 3. paramétert
                     //ahol látsz ilyen callback függvény hívást, ott a másik fájlba hívunk vissza
                     notExistsCallback();
                 }
@@ -45,7 +73,8 @@ const collectionExists = function (collectionName, exists, notExistsCallback) {
     });
 }
 
-const createCollection = function (newCollectionName, callback) {
+//létre hoz egy collectiont az átadott név alapján, és ha sikerült, akkor meghívja a 2. paraméterként átadott függvényt
+const createOurCollection = function (newCollectionName, callback) {
     MongoClient.connect(url, function (err, mongoClient) {
         if (err) throw err;
 
@@ -67,7 +96,7 @@ const createCollection = function (newCollectionName, callback) {
     });
 }
 
-//a collection nevét, és az entitás id-ját kérjük, és ha van ilyen elem az adatbázisban, akkor kiíratjuk az eredményt
+//a collection nevét, és az entitás id-ját kérjük, és ha van ilyen elem a collectionben, akkor kiíratjuk az eredményt
 //Ha nincs akkor azt hogy nincs
 const find = function (collectionName, entityId) {
     MongoClient.connect(url, async function (err, mongoClient) {
@@ -102,6 +131,8 @@ const find = function (collectionName, entityId) {
     });
 }
 
+
+//az adott collectionbe be "insertálunk egy objektumot (entity)
 const create = function (collectionName, entity) {
     MongoClient.connect(url, function (err, mongoClient) {
         if (err) throw err;
@@ -109,7 +140,7 @@ const create = function (collectionName, entity) {
         const database = mongoClient.db(dbName);
 
 
-        //insertáljuk a megkapott entitást a collectionbe
+        //az adott collectionbe 1db entityt rakunk be (más függvénnyel egyszerre többet is lehet)
         database.collection(collectionName).insertOne(entity)
             .then(result => {
                 console.log("Entity created");
@@ -125,6 +156,7 @@ const create = function (collectionName, entity) {
 }
 
 
+//az adott collectionbe, az adott id alapján befrissítjük az adatokat ami a 3. paraméterbe adtunk át
 const update = function (collectionName, entityId, newEntity) {
     MongoClient.connect(url, function (err, mongoClient) {
         if (err) throw err;
@@ -132,7 +164,7 @@ const update = function (collectionName, entityId, newEntity) {
         const database = mongoClient.db(dbName);
 
 
-        //id alapján frissítünk egy entitást a collectionben
+
         database.collection(collectionName).updateOne({_id: entityId}, newEntity)
             .then(result => {
                 console.log("Entity updated");
@@ -148,6 +180,7 @@ const update = function (collectionName, entityId, newEntity) {
 }
 
 
+//az adott collectionbe az adott id alapján törlünk egy entityt
 const deleteF = function (collectionName, entityId) {
     MongoClient.connect(url, function (err, mongoClient) {
         if (err) throw err;
@@ -155,7 +188,7 @@ const deleteF = function (collectionName, entityId) {
         const database = mongoClient.db(dbName);
 
 
-        //id alapján törlünk egy entitást a collectionből
+
         database.collection(collectionName).deleteOne({_id: entityId})
             .then(result => {
                 console.log("Entity deleted");
@@ -172,4 +205,4 @@ const deleteF = function (collectionName, entityId) {
 
 
 //ki exportáljuk a függvényeinket, hogy a másik fájl is használni tudja azokat
-module.exports = {createCollection, create, update, find, deleteF, dropCollection, collectionExists};
+module.exports = {createCollection: createOurCollection, create, update, find, deleteF, dropCollection: dropOurCollection, collectionExists};
--- mongohandlerTest.js
+++ mongohandlerTest.js
@@ -1,3 +1,8 @@
+
+
+//Az egész lefutás a fájl alján fog kezdődni, ahol ellenőrizzük, hogy a collection létezik e már az adatbázisban
+//A callback függvényeket át kell nézned, ahhoz hogy megértsd a működéseket
+
 //behúzom a fájlomat
 const mongohandler = require('./mongohandler');
 
@@ -7,27 +12,37 @@ const collectionName = "CollectionASD23";
 const entity = {_id: 1, 'name': 'Janos1', 'age': 21};
 const entity2 = {_id: 2, 'name': 'Janos2', 'age': 21};
 const entity3 = {_id: 3, 'name': 'Janos3', 'age': 21};
+
+//ahogy láttam update-nél meg kell adni a $set után, hogy mit akarunk frissíteni
 const newEntity = {$set: {'name': 'Agota1', 'age': 22}};
 
+
+//Ha meghívod ezt a függvényt, akkor a program várakozik 1mp-et
 async function waitOneSec() {
     await new Promise(resolve => setTimeout(resolve, 1000))
 }
 
 const crudCallback = async function () {
-//ha minden oké, és létezik a collection, akkor fog ez a függvény meghívódni, és elvégezni a CRUD művelteket az entitásokkaé
+
+    //létre hozunk 3 entitást
     mongohandler.create(collectionName, entity);
     mongohandler.create(collectionName, entity2);
     mongohandler.create(collectionName, entity3);
 
-
+    //várunk mindig 1-1mp-et, azért, hogy az async függvények le tudjanak futni addigra, mire elérünk a várakozás utánra
+    await waitOneSec();
     mongohandler.find(collectionName, entity._id);
+
     await waitOneSec();
     mongohandler.update(collectionName, entity._id, newEntity);
+
     await waitOneSec();
     mongohandler.find(collectionName, entity._id);
+
     await waitOneSec();
 
 
+    //töröljük az összes entitásunkat a collectionből
     mongohandler.deleteF(collectionName, entity._id);
     mongohandler.deleteF(collectionName, entity2._id);
     mongohandler.deleteF(collectionName, entity3._id);
